69b166798e99b95516fd1df6682acae83fd23824
Michael Anderson <michael.j.anderson@me.com>
2022-05-12 02:51:39 -0400

    Adding Basic Background, Will finish these pages tmrw

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -4,6 +4,7 @@ import NavigationBar from './components/navbar';
 import Footer from './components/footer/footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Courses from './pages/Courses';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
             <Route path ="/" element={<Home/>}/>
             <Route path ='/home' element={<Home/>}/>
             <Route path ='/projects' element={<Projects/>}/>
+            <Route path ='/courses' element={<Courses/>}/>
           </Routes>
     </Router>
   );
